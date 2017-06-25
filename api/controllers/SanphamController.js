9ee5db240577ba766d04a2ffd7d351acf2009f91
tranquocloc.qz <tranquocloc.qz@gmail.com>
2017-06-26 02:37:39 +0700

    viết báo cáo

--- api/controllers/SanphamController.js
+++ api/controllers/SanphamController.js
@@ -7,10 +7,11 @@
 var slug = require('url-slug');
 var fs = require('fs');
 var moment = require('moment');
+
 module.exports = {
+
 	danhsachGET: function (req, res) {
 		Sanpham.query('SELECT sanpham.id as spid, tensanpham,tenthietbi,tennhasanxuat,sanpham.createdAt, sanpham.updatedAt, sanpham.trangthai as sptrangthai FROM sanpham,thietbi,nhasanxuat WHERE sanpham.idnhasanxuat = nhasanxuat.id AND nhasanxuat.idthietbi = thietbi.id', function (err, sanpham) {
-			// res.send(sanpham);
 			return res.view('backend/sanpham/danhsach', {
 				layout: 'backend/layout/layout',
 				sanpham: sanpham,
@@ -95,7 +96,7 @@ module.exports = {
 								hinhsanpham: hinh,
 								title: 'Sửa sản phẩm ' + sp[0].tensanpham,
 							});
-						}						
+						}
 					});
 				});
 			});
@@ -104,47 +105,49 @@ module.exports = {
 
 	suaPOST: function (req, res) {
 		var path = require('path').resolve(sails.config.appPath, 'assets/images');
+		function saverc(sp, name) {
+			var rc = sp;
+			rc.anhdaidien = name;
+			rc.tensanpham = req.param('txt_tensp');
+			rc.soluong = req.param('txt_sl');
+			rc.gia = req.param('txt_gia');
+			rc.khuyenmai = req.param('txt_km');
+			rc.cauhinh = req.param('txt_cauhinh');
+			rc.mota = req.param('txt_noidung');
+			rc.idnhasanxuat = req.param('cbo_nhasx');
+			rc.slug = slug(req.param('txt_tensp'));
+			rc.save();
+		};
 		var name = slug(req.param('txt_tensp')) + '-' + Math.random().toString(36).substr(2, 5) + '.png';
-		var rc;
-		Sanpham.findOne({ id: req.param('id') }).exec({
-			err: function (err) {
-				return res.serverError(err);
-			},
-			success: function (sp) {
-				obj = sp;
-				req.file('ful_anhdaidien').upload({
-					saveAs: name,
-					dirname: path,
+		Sanpham.findOne({ id: req.param('id') }).exec(function (err, sp) {
+			req.file('ful_anhdaidien').upload({
+				saveAs: name,
+				dirname: path + '/anhdaidien/',
+			}, function (err, uploadedFiles) {
+				if (err)
+					return res.serverError(err);
+				if (_.isEmpty(uploadedFiles)) {
+					fs.renameSync(path + '/anhdaidien/' + sp.anhdaidien, path + '/anhdaidien/' + name);
+					saverc(sp, name);
+				} else {
+					fs.unlinkSync(path + '/anhdaidien/' + sp.anhdaidien);
+					saverc(sp, name);
+				}
+			});
+			Hinhsanpham.find({ idsanpham: sp.id }).exec(function (err, hsp) {
+				req.file('ful_anhsanpham').upload({
+					dirname: path + '/anhchitiet/',
 				}, function (err, uploadedFiles) {
 					if (err)
 						return res.serverError(err);
-					if (uploadedFiles == null) {
-						// fs.renameSync(path + '/' + sp.anhdaidien, path + '/' + name);
-						return res.json({
-							message: 'k chon',
-							anh: sp,
-						});
-					} else {
-						// fs.unlinkSync(path + '/' + sp.anhdaidien);
-						return res.json({
-							message: 'co chon',
-							anh: sp,
-						});
+					if (!_.isEmpty(uploadedFiles)) {
+						rc = hsp;
+						
+						fs.unlinkSync(path + '/anhchitiet/' + sp.anhdaidien);						
 					}
 				});
-				rc = sp;
-				rc.anhdaidien = name;
-				rc.tensanpham = req.param('txt_tensp');
-				rc.soluong = req.param('txt_sl');
-				rc.gia = req.param('txt_gia');
-				rc.khuyenmai = req.param('txt_km');
-				rc.cauhinh = req.param('txt_cauhinh');
-				rc.mota = req.param('txt_noidung');
-				rc.idnhasanxuat = req.param('cbo_nhasx');
-				rc.slug = slug(req.param('txt_tensp'));
-				rc.save();
-				// return res.redirect('/admin/sanpham/sua/' + sp.id);
-			},
+			});
+			return res.redirect('/admin/sanpham/sua/' + sp.id);
 		});
 	},
 	xoaPOST: function (req, res) {
